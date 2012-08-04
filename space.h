5c09545d255335545ba76bd6cd1a828d68ef3939
robsonde <d.roc@asinine.net.nz>
2012-08-05 11:32:26 +1200

    moved the setup of cargo base price to the space.h
    the static data of cargo names and base price is now in the .h

--- space.h
+++ space.h
@@ -20,12 +20,74 @@ int contraband;
 
 
 
+
+
 struct econ
 {
 int price;
 int avil;
 };
 
+
+
+
+
+
+
+
+
+//setup of the cargo types and prices.
+struct cargo cargo_types[]={
+{.name="Water",
+.base_price=50,
+.contraband=0,
+},
+
+{.name="Food",
+.base_price=100,
+.contraband=0,
+},
+
+{.name="Furs",
+.base_price=300,
+.contraband=0,
+},
+
+{.name="Ore",
+.base_price=400,
+.contraband=0,
+},
+
+{.name="Games",
+.base_price=200,
+.contraband=0,
+},
+
+{.name="Firearms",
+.base_price=900,
+.contraband=1,
+},
+
+{.name="Medicine",
+.base_price=500,
+.contraband=0,
+},
+
+{.name="Machines",
+.base_price=1100,
+.contraband=0,
+},
+
+{.name="Narcotics",
+.base_price=3000,
+.contraband=1,
+},
+
+{.name="Robots",
+.base_price=5000,
+.contraband=0,
+},
+};
 
 
 
@@ -41,6 +103,8 @@ int cap;
 struct econ cargo [num_cargo_types];
 int num_cargo;
 };
+
+
 
 
 
@@ -74,10 +138,4 @@ int pirates;
 struct location pos;
 struct econ cargo_types [num_cargo_types];
 };
-
-
-
-
-
-
 
