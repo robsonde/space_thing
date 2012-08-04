a9bab05a14e7b890f61b0d62548ab9b28d291d33
robsonde <d.roc@asinine.net.nz>
2012-08-04 21:08:38 +1200

    setup of all the basic cargo types and base prices
    data setup of cargo, name , price, contraband

--- space.c
+++ space.c
@@ -37,13 +37,49 @@ struct planet planets[]={
 
 //setup of te cargo types and prices.
 struct cargo cargo_types[]={
-{.name="water",
+{.name="Water",
 .base_price=50,
 .contraband=0,
 },
 
-{.name="food",
+{.name="Food",
 .base_price=100,
+.contraband=0,
+},
+
+{.name="Furs",
+.base_price=300,
+.contraband=0,
+},
+
+{.name="Ore",
+.base_price=400,
+.contraband=0,
+},
+
+{.name="Games",
+.base_price=200,
+.contraband=0,
+},
+{.name="Firearms",
+.base_price=900,
+.contraband=1,
+},
+{.name="Medicine",
+.base_price=500,
+.contraband=0,
+},
+{.name="Machines",
+.base_price=1100,
+.contraband=0,
+},
+{.name="Narcotics",
+.base_price=3000,
+.contraband=1,
+},
+
+{.name="Robots",
+.base_price=5000,
 .contraband=0,
 },
 
